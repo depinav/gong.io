efa17a316f15e762b396be96387afe9e7250b0d9
Victor DePian <depinav@gmail.com>
2025-10-06 11:23:55 -0400

    Added biome for linting

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,16 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const container = document.getElementById("root");
+
+if (container) {
+	const root = createRoot(container);
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+} else {
+	// Handle the error, for example:
+	console.error("Failed to find the root element.");
+}
